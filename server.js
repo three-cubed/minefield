dc1bda327b2e0c4a658aa75a02a6624ed5068f61
three-cubed <72698356+three-cubed@users.noreply.github.com>
2022-02-04 15:57:51 +0000

    adding favicon

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const app = express();
 const PORT = process.env.PORT || 3333;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
+
 const router = require('./routes.js');
 app.use('/', router);
+
+const favicon = require('serve-favicon');
+app.use(favicon('./favicon_minefield.png'));
 
 app.listen(PORT);
 
