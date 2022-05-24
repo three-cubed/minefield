1460439349ff2abadff02af665b47e3cb727716c
three-cubed <72698356+three-cubed@users.noreply.github.com>
2022-05-24 17:17:04 +0100

    styling for instruction page

--- routes.js
+++ routes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('start.ejs');
-})
+});
 
 router.get('/play', (req, res) => {
     res.render('play.ejs');
-})
+});
 
 router.get('/instructions', (req, res) => {
     res.render('instructions.ejs');
-})
+});
 
 router.get('/narrow', (req, res) => {
     res.render('narrow.ejs');
-})
+});
 
 module.exports = router;
